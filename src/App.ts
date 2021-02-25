a3ac44fd2c16825b02b3c156e01d8196395a54b4
kyanag <gongzuo_yk@foxmail.com>
2021-02-25 17:50:56 +0800

    ts

--- src/App.ts
+++ src/App.ts
@@ -1,17 +1,20 @@
-const httpClient = require("request");
+fetch
 const { filter, concatMap } = require("rxjs/operators");
 const EventEmitter = require('events');
 const lodash = require("lodash");
+const Queue = Array;
+const { createWorkflow} = require("./Core.js");
 
-const { createWorkflow, Queue } = require("./Core.js");
-
-Function.prototype.addDecorator = function(now){
-    let last = this;
-    return function(...args){
+Function.prototype.addDecorator = function(now: Function){
+    let last:Function = this;
+    return function(...args: any){
         let _ = last.call(this, ...args);
         return now.call(this, _);
     }
 }
+
+
+
 /* 
     队列内容
     {
@@ -26,14 +29,48 @@ Function.prototype.addDecorator = function(now){
         _retry: 0,   //重试次数
     },
 */
+interface Resource{
+    request: Request,
+    response?: Response,
+    _topics: Array<string>,
+    _extra_attributes: any,
+    _retry: Number, 
+}
+
+
+interface Events{
+    [index: string]: Function
+    
+    ["app.ready"] : Function,                //App ready
+    ["app.before-stop"] : Function,          //App 停止
+    ["app.error"] : Function,                //App 退出Rx循环事件
+    
+    ["resource.push"] : Function,            //resource 入队
+    ["resource.shift"] : Function,           //resource 出队 - 超过最大次数或者被主动抛弃
+    ["resource.before-fetch"] : Function,    //resource 发出请求之前
+    ["resource.fetch-success"] : Function,   //resource response
+    ["resource.fetch-fail"] : Function,      //resource 请求失败
+
+    ["extract.success"] : Function,          //抽取器 抽取数据成功
+    ["extract.fail"] : Function,             //抽取器 抽取失败
+}
+
+interface Config{
+    id: string,
+    entry: Array<string|Resource>,
+    max_retry_num: Number,
+    interval: Number,
+    timeout: Number,
+    listeners: Events,
+}
 
 class App extends EventEmitter{
-    id
-    max_retry_num = 5
-    interval = 1000             
-    timeout = 10000
+    id: string
+    max_retry_num: Number = 5
+    interval: Number = 1000             
+    timeout: Number = 10000
 
-    constructor(config){
+    constructor(config: Config){
         super();
 
         this.id = config.id;
@@ -41,16 +78,17 @@ class App extends EventEmitter{
         this.interval = config.interval;
         this.timeout = config.timeout;
 
-        //加载事件
+        //绑定事件
         let listeners = config.listeners || {};
         for(let name in listeners){
-            this.on(name, listeners[name].bind(this));
+            let handler = listeners[name];
+            this.on(name, handler.bind(this));
         }
 
         //初始化队列
         this._queue = new Queue();
-        config.entry.forEach( (url) => {
-            this.addResource(url);
+        config.entry.forEach( (resource) => {
+            this.addResource(resource);
         });
 
         this.config = config;
@@ -63,19 +101,13 @@ class App extends EventEmitter{
      * @param {Object} extra_attributes     附加数据
      * @param {String} save_as              保存到地址
      */
-    addResource(resource, topics = null, extra_attributes = null){
+    addResource(resource: Resource, topics: Array<string> = [], extra_attributes = null){
         if(typeof(topics) == "string"){
             topics = [topics];
         }
         if(typeof(resource) == 'string'){
             resource = {
-                request:{
-                    method: "get",
-                    url: resource,
-                    headers: {},
-                    timeout: 5000,
-                    encoding: null,
-                },
+                request: new Request(resource),
                 _topics: topics,                            //强制主题
                 _extra_attributes: extra_attributes,        //附加属性
                 _retry: 0,                                  //重试次数
@@ -101,7 +133,7 @@ class App extends EventEmitter{
      * fetcher:fetch 下载
      * @param {Object} resource 
      */
-    async fetch(resource){
+    async fetch1(resource){
         return await new Promise((resolve, reject) => {
             try{
                 let request = resource.request;
